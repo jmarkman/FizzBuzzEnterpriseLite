9e78f7f1da179c17c7d06668a52731d13dffea2d
Jonathan Markman <jmarkman446@gmail.com>
2020-12-30 14:58:39 -0500

    Add project files.

---
+++ ClearMeasureFizzBuzz/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FizzBuzzEnterprise;
+
+namespace ClearMeasureFizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fb = new FizzBuzz();
+
+            fb.PerformFizzBuzz(int.MaxValue);
+        }
+    }
+}
---
+++ FizzBuzzEnterprise/FizzBuzz.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FizzBuzzEnterprise
+{
+    public class FizzBuzz
+    {
+        public FizzBuzz()
+        {
+                
+        }
+
+        // upperBound is int for however far you want to calculate
+        public void PerformFizzBuzz(int upperBound)
+        {
+            if (upperBound <= 1)
+            {
+                throw new ArgumentException("Supplied value less than starting position, FizzBuzz will not occur");
+            }
+
+            for (int i = 1; i <= upperBound; i++)
+            {
+                bool divisibleByThree = i % 3 == 0;
+                bool divisibleByFive = i % 5 == 0;
+
+                if (divisibleByThree && divisibleByFive)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (divisibleByThree)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (divisibleByFive)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
