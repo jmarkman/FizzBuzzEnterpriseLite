66afe1df80c476638032e3913c7d68c415cc57e2
Jonathan Markman <jmarkman446@gmail.com>
2020-12-30 15:38:55 -0500

    implement yield version of hardcoded fizzbuzz

--- FizzBuzzEnterprise/FizzBuzz.cs
+++ FizzBuzzEnterprise/FizzBuzz.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FizzBuzzEnterprise
 {
@@ -37,6 +38,42 @@ namespace FizzBuzzEnterprise
                 else
                 {
                     Console.WriteLine(i);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Performs a normal FizzBuzz calculation using hardcoded conditions
+        /// </summary>
+        /// <param name="upperBound">Where the FizzBuzz iteration should stop</param>
+        /// <returns></returns>
+        public IEnumerable<string> YieldFizzBuzz(int upperBound)
+        {
+            if (upperBound <= 1)
+            {
+                throw new ArgumentException("Supplied value less than starting position, FizzBuzz will not occur");
+            }
+
+            for (int i = 1; i <= upperBound; i++)
+            {
+                bool divisibleByThree = i % 3 == 0;
+                bool divisibleByFive = i % 5 == 0;
+
+                if (divisibleByThree && divisibleByFive)
+                {
+                    yield return "FizzBuzz";
+                }
+                else if (divisibleByThree)
+                {
+                    yield return "Fizz";
+                }
+                else if (divisibleByFive)
+                {
+                    yield return "Buzz";
+                }
+                else
+                {
+                    yield return i.ToString();
                 }
             }
         }
