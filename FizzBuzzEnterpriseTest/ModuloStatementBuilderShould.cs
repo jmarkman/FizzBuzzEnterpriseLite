88bd847fb846c686dcd94638e6358254b02af5a1
Jonathan Markman <jmarkman446@gmail.com>
2020-12-31 18:03:52 -0500

    add test suite for ModuloStatementBuilder

---
+++ FizzBuzzEnterpriseTest/ModuloStatementBuilderShould.cs
@@ -0,0 +1,17 @@
+﻿using FizzBuzzEnterprise;
+using NUnit.Framework;
+
+namespace FizzBuzzEnterpriseTest
+{
+    [TestFixture]
+    public class ModuloStatementBuilderShould
+    {
+        private ModuloStatementBuilder _statementBuilderUnderTest;
+
+        [SetUp]
+        public void Setup()
+        {
+            _statementBuilderUnderTest = new ModuloStatementBuilder();
+        }
+    }
+}
